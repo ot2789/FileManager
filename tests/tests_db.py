fc953d3ad86b2ef0b76783c43505ab4f608a0f2c
ot2789 <32983454+ot2789@users.noreply.github.com>
2022-06-29 14:45:42 +0300

    CH 11.2 Added database tests for role model.

--- tests/tests_db.py
+++ tests/tests_db.py
@@ -47,8 +47,10 @@ def user(db, db_session):
 
 @pytest.mark.usefixtures("erase_db")
 class TestSuite:
-    def test_empty_database(self, db_session):
-        assert db_session.query(DataBase.User).first() is None
+    def test_init_database(self, db_session):
+        user = db_session.query(DataBase.User).first()
+        assert user is not None
+        assert user.email == os.environ['ADMIN_EMAIL']
 
     def test_insert_user(self, db: DataBase, db_session):
         db_session.add(db.User(**TEST_USER))
@@ -73,4 +75,90 @@ class TestSuite:
         db_session.commit()
         sessions = db_session.query(db.Session).all()
         assert len(sessions) == 0
+
+    def test_user_method_roles(self, db: DataBase, db_session, user: DataBase.User):
+        methods = [db.Method('meth1'), db.Method('meth2'), db.Method('meth3')]
+        for m in methods:
+            db_session.add(m)
+        roles = [
+            db.Role('role2', [user], [methods[1], methods[2]]),
+            db.Role('role1', [user], [methods[0], methods[1]])
+        ]
+        # We reverse this to assign the user to role1
+        db_session.add_all(roles)
+        db_session.commit()
+        roles.reverse()
+
+        user = db_session.query(DataBase.User).filter_by(email=TEST_USER['email']).first()
+        assert user.role.name == 'role1'
+        role = db_session.query(DataBase.Role).filter_by(name='role1').first()
+        role_methods = [m.method.name for m in role.methods]
+        assert methods[0].name in role_methods
+        assert methods[1].name in role_methods
+        assert methods[2].name not in role_methods
+
+        def check_roles(method_name, expected_roles):
+            api_methods = [mr.role.name for mr in \
+                           db_session.query(DataBase.Method).filter_by(name=method_name).first().roles]
+            if expected_roles:
+                for r in expected_roles:
+                    assert r in api_methods
+            else:
+                assert len(api_methods) == 0
+
+        check_roles('meth1', ('role1',))
+        check_roles('meth2', ('role1', 'role2',))
+        check_roles('meth3', ('role2',))
+
+        # Changing user role
+        user.role = db_session.query(DataBase.Role).filter_by(name='role2').first()
+        db_session.commit()
+        user = db_session.query(DataBase.User).filter_by(email=TEST_USER['email']).first()
+
+        # Checking if it has been correctly updated.
+        assert user.role.name == 'role2'
+        role = db_session.query(DataBase.Role).filter_by(name='role2').first()
+        role_methods = [m.method.name for m in role.methods]
+        assert methods[0].name not in role_methods
+        assert methods[1].name in role_methods
+        assert methods[2].name in role_methods
+
+        # Deleting role1 should not delete user
+        db_session.query(DataBase.Role).filter_by(name='role1').delete()
+        db_session.commit()
+        user = db_session.query(DataBase.User).filter_by(email=TEST_USER['email']).first()
+        assert user.email == TEST_USER['email']
+
+        check_roles('meth1', tuple())
+        check_roles('meth2', ('role2',))
+        check_roles('meth3', ('role2',))
+
+        # Here we just want to decouple meth2 from role2
+        method_roles = db_session.query(DataBase.MethodRole).filter_by(method=methods[1], role=roles[1])
+        assert len(method_roles.all()) == 1
+        method_roles.delete()
+        db_session.commit()
+
+        check_roles('meth1', tuple())
+        check_roles('meth2', tuple())
+        check_roles('meth3', ('role2',))
+
+        # Deleting role2 should delete user
+        db_session.query(DataBase.Role).filter_by(name='role2').delete()
+        db_session.commit()
+        user = db_session.query(DataBase.User).filter_by(email=TEST_USER['email']).first()
+        assert user is None
+
+        check_roles('meth1', tuple())
+        check_roles('meth2', tuple())
+        check_roles('meth3', tuple())
+
+        existing_roles = [u.name for u in db_session.query(DataBase.User).all()]
+        for r in ('role1', 'role2',):
+            assert r not in existing_roles
+
+        existing_methods = [m.name for m in db_session.query(DataBase.Method).all()]
+        for m in ('meth1', 'meth3',):
+            assert m in existing_methods
+            db_session.query(DataBase.Method).filter_by(name=m).delete()
 
